8affbc48a3602311a7b08bd09217eb5bffe2bf70
Hassaan1814 <hassaanjaved82@triconv.com>
2023-01-21 19:30:27 +0500

    form

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import "./form.scss";
 const Form = () => {
-  return <div></div>;
+  return (
+    <section className="Form__section">
+      <div className="Form__section-container">
+        <div className="form__section-form">
+          <form action="">
+            <input type="text" placeholder="Full name" />
+          </form>
+        </div>
+        <div className="form__section-clear"></div>
+      </div>
+    </section>
+  );
 };
 
 export default Form;
